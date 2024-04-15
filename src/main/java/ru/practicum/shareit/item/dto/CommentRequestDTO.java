265f566191186d9758d688ace151ec9cb65e09dd
s.kukhtanov <s.kukhtanov@tinkoff.ru>
2024-04-15 14:05:41 +0300

    homework 14: review fix v2

--- src/main/java/ru/practicum/shareit/item/dto/CommentRequestDTO.java
+++ src/main/java/ru/practicum/shareit/item/dto/CommentRequestDTO.java
@@ -3,9 +3,11 @@ package ru.practicum.shareit.item.dto;
 import javax.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 
 @Data
 @Builder
+@Jacksonized
 public class CommentRequestDTO {
 
   @NotEmpty
